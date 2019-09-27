83a4113ce15fbb24698b6b8ad796f2f6ec69a88e
eonbl <ben9604@gmail.com>
2019-09-27 14:18:12 -0400

    Fix optimistic unchoking

--- skt_t1std.py
+++ skt_t1std.py
@@ -30,61 +30,51 @@ class SKT_T1Std(Peer):
 
         This will be called after update_pieces() with the most recent state.
         """
-        needed = lambda i: self.pieces[i] < self.conf.blocks_per_piece
-        needed_pieces = filter(needed, range(len(self.pieces)))
-        np_set = set(needed_pieces)  # sets support fast intersection ops.
-
+        need_list = []
+        for i in range(len(self.pieces)):
+            if self.pieces[i] < self.conf.blocks_per_piece:
+                need_list.append(i)
+        need_set = set(need_list)
 
         logging.debug("%s here: still need pieces %s" % (
-            self.id, needed_pieces))
-
+            self.id, need_list))
         logging.debug("%s still here. Here are some peers:" % self.id)
         for p in peers:
             logging.debug("id: %s, available pieces: %s" % (p.id, p.available_pieces))
-
         logging.debug("And look, I have my entire history available too:")
-        logging.debug("look at the AgentHistory class in history.py for details")
         logging.debug(str(history))
 
-        requests = []   # We'll put all the things we want here
+        requests = []
         # Symmetry breaking is good...
-        random.shuffle(needed_pieces)
-        
-        # Sort peers by id.  This is probably not a useful sort, but other 
-        # sorts might be useful
+        random.shuffle(need_list)
 
-        # loop through every piece you need
-        for needed in needed_pieces:
-            # find out which peers have each piece by looping through the piece availabilities of peers
+        # Store number of peers with our needed pieces in dictionary
+        for needed in need_list:
             peers_with_piece = filter(lambda peer: needed in peer.available_pieces, peers)
-            # store number of peers with piece in dictionary
             self.piece_availabilities[needed] = len(peers_with_piece)
 
-        # request pieces from all peers, up to self.max_requests from each
-        # use rarest-first strategy for requesting pieces from peers
+        # Request pieces from all peers, up to self.max_requests from each
+        # Use rarest-first strategy for requesting pieces from peers
         for peer in peers:
             av_set = set(peer.available_pieces)
-            isect = av_set.intersection(np_set)
+            isect = av_set.intersection(need_set)
             n = min(self.max_requests, len(isect))
-            # sort this based on the pieces in your piece availability dictionary
             sorted_by_rarest = sorted(isect, key=lambda piece: self.piece_availabilities[piece])
 
             if sorted_by_rarest is None:
-                print "No request; no peers have our needed pieces"
+                logging.debug("No request; no peers have our needed pieces")
                 return []
 
             # This would be the place to try fancier piece-requesting strategies
             # to avoid getting the same thing from multiple peers at a time.
 
-            # take the top n pieces from your dictionary
+            # Take the top n pieces from the dictionary and get the next-needed blocks in order
             for piece_id in sorted_by_rarest[:n]:
-                # aha! The peer has this piece! Request it.
-                # which part of the piece do we need next?
-                # (must get the next-needed blocks in order)
                 start_block = self.pieces[piece_id]
                 r = Request(self.id, peer.id, piece_id, start_block)
                 requests.append(r)
 
+        logging.debug("Requests from {}: {}".format(self.id, requests))
         return requests
 
     def uploads(self, requests, peers, history):
@@ -103,7 +93,7 @@ to any given peer.
         logging.debug("%s again.  It's round %d." % (
             self.id, round))
 
-        # don't upload if you don't receive requests
+        # Don't upload if you don't receive requests
         if not requests:
             return []
 
@@ -111,39 +101,14 @@ to any given peer.
         # For example, history.downloads[round-1] (if round != 0, of course) has a list of Download objects for each Download to this peer in the previous round.
 
         requesters_ids = map(lambda request: request.requester_id, requests)
-        peers_to_unchoke = set()
 
-        # if first round, give unchoke spot to random agent. if later round, give to highest upload bandwith to us
+        # If first round, give unchoke spot to random agent. If later round, give to agent who gave us highest bandwidth
         if round == 0:
-            # randomly pick 4 users requesting pieces from us, and give each of them equal bandwidth
+            # Randomly pick 4 users requesting pieces from us, and give each of them equal bandwidth
             chosen_peer_ids = random.sample(requesters_ids, 4)
             peers_to_unchoke = chosen_peer_ids
         else:
-            # pick the top 3 people who gave us the highest upload bandwidth in the past period
-            # prev_upload = history.downloads[:-1]
-            # BEN
-            # last_round_uploads = [v[:-1] for v in history.uploads] # this is a list of uploads
-            # last_round_uploads_flattened = [upload for sublist in last_round_uploads for upload in sublist] # just user uploads
-            # my_uploaders = filter(lambda upload: upload.to_id == self.id, last_round_uploads_flattened)
-            # find the top 3 peers who gave us the most download bandwidth (i.e. blocks)
-            # top_peer_uploads = sorted(my_uploaders, key=lambda upload: upload.bw)[-3:]
-
-            #MICHELE
-            # print(history)
-            # print("history", history.downloads)
-            # print("history", history.uploads)
-            # prev_uploads = history.uploads[-1]
-            # print("previous uploads", prev_uploads)
-            # my_uploaders = filter(lambda upload: upload.to_id == self.id, prev_uploads)
-            # print("my uploaders", my_uploaders)
-            # top_peer_uploads = sorted(my_uploaders, key=lambda upload:upload.bw)[-3:]
-            # top_peer_ids = map(lambda uploader: uploader.from_id, top_peer_uploads)
-            # peers_to_unchoke = top_peer_ids
-            # print("hi", peers_to_unchoke)
-            # peers array with 3 regular peers and save one for optimistic guy
-
-            # print("Downloads", history.downloads)
-            # print("Uploads", history.uploads)
+            # Pick the top 3 people who gave us the highest upload bandwidth in the past period
             downloads = history.downloads[-1]
             down_bw = defaultdict(int)
             down_set = set()
@@ -152,23 +117,19 @@ to any given peer.
                 down_set.add(download.from_id)
             top_downloads = list(down_set)
             top_downloads.sort(key=lambda x: down_bw[x], reverse=True)
-            peers_to_unchoke = top_downloads
+            peers_to_unchoke = top_downloads[-3:]
 
-        # Optimistic unchoking every 3 rounds
+        # Optimistic unchoking every 3 rounds: randomly choose an agent who isn't already in peers_to_unchoke
         if round % 3 == 0 and len(requesters_ids) > len(peers_to_unchoke):
-            # randomly choose a person from requesters_ids
-            # make sure that person is not someone you're already unchoking
-            self.optimistic_unchoked_peer = random.sample(requesters_ids, 1)
+            self.optimistic_unchoked_peer = random.choice(requesters_ids)
             while self.optimistic_unchoked_peer in peers_to_unchoke:
-                self.optimistic_unchoked_peer = random.sample(requesters_ids, 1)
+                self.optimistic_unchoked_peer = random.choice(requesters_ids)
 
-        # if we have spots left
+        # If we have spots left, add the peer from optimistic unchoking
         if len(peers_to_unchoke) < 4:
             peers_to_unchoke.append(self.optimistic_unchoked_peer)
 
-        # print("Unchoke: ", peers_to_unchoke)
         bws = even_split(self.up_bw, len(peers_to_unchoke))
-        # create actual uploads out of the list of peer ids and bandwidths
         uploads = [Upload(self.id, peer_id, bw)
                    for (peer_id, bw) in zip(peers_to_unchoke, bws)]
             
